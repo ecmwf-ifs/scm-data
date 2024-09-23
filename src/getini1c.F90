0f7c27b259c624605c638c9135e675eb3146bc41
Antonino Bonanni <antonino.bonanni@ecmwf.int>
2024-09-23 12:41:16 +0000

    copyright headers

--- src/getini1c.F90
+++ src/getini1c.F90
@@ -1,3 +1,13 @@
+! (C) Copyright 2024- ECMWF.
+!
+! This software is licensed under the terms of the Apache Licence Version 2.0
+! which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+!
+! In applying this licence, ECMWF does not waive the privileges and immunities
+! granted to it by virtue of its status as an intergovernmental organisation nor
+! does it submit to any jurisdiction.
+
+
 !!! use ldd <executable> to figure out which libraries have been used in the executable !!!
 !....................................................................... 
 !     GETS A PROFILE OF ATMOSPHERIC AND SURFACE VARIABLES
